f568ea0b7d3236d71b43f56e8c2cdb88c8a840be
Anandu R <ananduraj32@gmail.com>
2020-06-21 15:46:03 +0530

    meh

--- Navigation.R
+++ Navigation.R
@@ -1,5 +1,6 @@
 if(getwd()!="~/GitLab/odu_komban_schd_algorithm"){
   setwd("~/GitLab/odu_komban_schd_algorithm")
 }
+library(rmarkdown)
 rmarkdown::render("Rmd/algorithm_generation.Rmd",output_file = "../algorithm_generation.pdf")
 
